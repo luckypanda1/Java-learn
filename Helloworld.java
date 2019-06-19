71e5685017c0a4734e72d90d70ca0e634218161f
luckypanda1 <51992846+luckypanda1@users.noreply.github.com>
2019-06-19 18:05:21 +0800

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,10 +1,11 @@
 package com.panda;
-
+import java.util.*;
 public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		System.out.println("Hello world!");
+		System.out.println("Hello, it's:");
+		System.out.println(new Date());
 	}
 
 }
